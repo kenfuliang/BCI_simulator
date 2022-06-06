14b5af090c2bbd80b58cd7171ac10fb2dce646e8
kenfuliang <kenfuliang@ucla.edu>
2022-06-06 11:37:17 -0700

    upload scripts

---
+++ scripts/rerun_all.sh
@@ -0,0 +1,11 @@
+
+python3 run.py --decoder=hand
+
+python3 run.py --decoder=FIT
+
+python3 run.py --decoder=ReFIT
+
+python3 run.py --decoder=FORCE
+
+python3 run.py --decoder=VKF
+
---
+++ scripts/retrain_all.sh
@@ -0,0 +1,9 @@
+python3 run.py --decoder=hand --learning_epochs=100 
+
+python3 run.py --decoder=FIT --learning_epochs=100 --retrain_decoder=1 
+
+python3 run.py --decoder=ReFIT --learning_epochs=100 --retrain_decoder=1 
+
+python3 run.py --decoder=FORCE --learning_epochs=100 --retrain_decoder=1 
+
+python3 run.py --decoder=VKF --learning_epochs=100 --retrain_decoder=1 
